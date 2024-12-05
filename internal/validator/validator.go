44d30042b3b292d584f264e5589b346cc67f33e9
VaheAA <vahe.abov@gmail.com>
2024-12-06 01:32:43 +0400

    add form validation and post request

---
+++ internal/validator/validator.go
@@ -0,0 +1,19 @@
+package validator
+
+type Validator struct {
+	FieldErrors map[string]string
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0
+}
+
+func (v *Validator) AddFieldError(key, message string) {
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+
+	if _, exists := v.FieldErrors[key]; !exists {
+		v.FieldErrors[key] = message
+	}
+}
